12d8dfbeebeab99af4ad4c94bc047a50094daad1
Scaramir <maxotto45@gmail.com>
2024-04-18 15:55:45 +0200

    Day 1. Installing dicom_parser
    Co-authored-by: floherzler <floherzler@users.noreply.github.com>

--- src/session1.py
+++ src/session1.py
@@ -1,1 +1,11 @@
+# pip install dicom-parser
+
 from dicom_parser import Image
+
+img = Image('../data/series-00000/image-00000.dcm') # just the first one
+# https://www.dicomlibrary.com/dicom/dicom-tags/
+
+# get tag value by name
+# ATTENTION: no white spaces allowed
+image_type = img.header.raw["ImageType"].value
+
