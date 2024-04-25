104852ba35526d3b37fbc5fd1ad8d57d259a9961
Scaramir <maxotto45@gmail.com>
2024-04-25 14:16:33 +0200

    session1

--- src/session1.py
+++ src/session1.py
@@ -1,4 +1,5 @@
 # pip install dicom-parser
+from pathlib import Path
 
 from dicom_parser import Image
 
@@ -8,4 +9,41 @@ img = Image('../data/series-00000/image-00000.dcm') # just the first one
 # get tag value by name
 # ATTENTION: no white spaces allowed
 image_type = img.header.raw["ImageType"].value
+
+# Exercise
+# Imaging Technique? -> CT
+img.header.raw["Modality"].value
+# Body Part? -> abdomen
+img.header.raw["StudyDescription"].value
+# When image taken? -> 20061012
+img.header.raw["AcquisitionDate"].value
+# Patient Position in CT machine? -> FFS feet first supine
+img.header.raw["PatientPosition"].value
+# Code for Opposite Orientation? -> HFS head first supine
+# XYZ Image size in millimeters?
+
+# Retrieve Pixel Spacing
+pixel_spacing = img.header.raw[(0x0028, 0x0030)].value # oh wow, tuples, ja? xD
+pixel_spacing_x, pixel_spacing_y = map(float, pixel_spacing)
+rows = img.header.raw["Rows"].value
+columns = img.header.raw["Columns"].value
+# number of dcm files in the /data folder?
+
+# get number of slices in the folder
+slices = len(list(Path("../data/series-00000").glob("*.dcm")))
+
+# spacing between slices
+spacing = img.header.raw[(0x0018, 0x0088)].value
+x = rows * pixel_spacing_x
+y = columns * pixel_spacing_y
+z = slices * spacing
+
+print(f"Image size: {x} x {y} x {z} mm")
+
+# tag for patient name? do we have that information?
+img.header.raw["PatientName"].value # Anonymized here
+
+# didn't get to do this
+# compression format for pixel data?
+# bit depth?
 
