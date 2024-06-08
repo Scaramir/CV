067de8e1c754aec19492ad206e0663b07005282b
Scaramir <maxotto45@gmail.com>
2024-06-08 19:21:19 +0200

    week4 notes for simple CNN

--- src/day4_max_notes.py
+++ src/day4_max_notes.py
@@ -37,14 +37,14 @@ trainset = torchvision.datasets.MNIST(
     root="./data", train=True, download=True, transform=transform
 )
 trainloader = torch.utils.data.DataLoader(
-    trainset, batch_size=BATCH_SIZE, shuffle=True, num_workers=2
+    trainset, batch_size=BATCH_SIZE, shuffle=True, num_workers=0
 )
 
 testset = torchvision.datasets.MNIST(
     root="./data", train=False, download=True, transform=transform
 )
 testloader = torch.utils.data.DataLoader(
-    testset, batch_size=BATCH_SIZE, shuffle=False, num_workers=2
+    testset, batch_size=BATCH_SIZE, shuffle=False, num_workers=0
 )
 
 # functions to show an image
