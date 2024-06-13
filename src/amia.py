2d67ebd33e9e9e63931c99521c1f265335efd9b2
Maike Herkenrath <herkenrath@campus.tu-berlin.de>
2024-06-13 12:44:30 +0200

    use dict, split train/test and use mean / std function
    Co-authored-by: floherzler <floherzler@users.noreply.github.com>

--- src/amia.py
+++ src/amia.py
@@ -7,6 +7,7 @@ from torch.utils.data import Dataset
 from torchvision import datasets, transforms
 from torchvision.io import read_image
 from torchvision.transforms import v2
+import torchvision.tv_tensors as tv
 import medmnist
 from medmnist import ChestMNIST, DermaMNIST, INFO, Evaluator
 import torch
@@ -20,6 +21,7 @@ from torchinfo import summary
 import torchvision
 from matplotlib import pyplot as plt
 import numpy as np
+import json
 
 import torch.nn.functional as F
 import torch.utils.data as data
@@ -60,15 +62,16 @@ def set_seeds(seed=123420):
     return
 
 
-def get_mean_and_std(data_dir, print=False, leave_pbar=False):
+def get_mean_and_std(data_dir, img_list, print=False, leave_pbar=False):
     """
     Acquire the mean and std color values of all images (RGB-values) in the training set.
     inpupt: "data_dir" string
     output: mean and std Tensors
     """
-    # Load the data set
+    # Load the data set matching img_list
     dataset = datasets.ImageFolder(
-        os.path.join(data_dir, "train"), transform=transforms.ToTensor()
+        [id for id in os.listdir(data_dir) if id in img_list],
+        transform=transforms.ToTensor()
     )
     dataloader = torch.utils.data.DataLoader(
         dataset=dataset, batch_size=1, num_workers=0
@@ -98,40 +101,78 @@ def get_mean_and_std(data_dir, print=False, leave_pbar=False):
 class XRayImageDataset(Dataset):
     def __init__(
         self,
-        dataframe,
+        dict,
+        img_size,
         img_dir,
         mean=None,
         std=None,
-        target_transform=None,
+        transform_norm=None,
     ):
-        self.img_labels = dataframe
+        self.dict = dict
+        self.keys = list(dict.keys())
+        self.img_size = img_size
         self.img_dir = img_dir
         self.mean = mean
         self.std = std
-        self.transform_norm = transforms.Compose(
-            [
-                transforms.Resize((224, 224), antialias=True),
-                transforms.Normalize(self.mean, self.std),
-            ]
-        )
-        self.target_transform = target_transform
+        self.transform_norm = transform_norm
 
     def __len__(self):
-        return len(self.img_labels)
+        return len(self.keys)
 
     def __getitem__(self, idx):
-        img_id = self.img_labels.iloc[idx]["image_id"]
+        img_id = self.keys[idx]
         img_path = os.path.join(self.img_dir, img_id) + ".png"
         image = read_image(img_path).float()  # PyTorch function, no need to change
-        label = self.img_labels.iloc[idx]["class_id"]  # class_id column
         image = self.transform_norm(image)
-        return image, label
+        # https://pytorch.org/tutorials/intermediate/torchvision_tutorial.html
+        
+
+        box_list = []
+        label_list = []
+        area_list = []
+        iscrowd_list = []
+        # get all boxes of all radiologists
+        # structure of the dict
+        # img_id: {
+        #     "classes": [
+        #         class_id: [
+        #            rad_id: [[bbox],[bbox]]]
+
+        for class_id in self.dict[img_id]["classes"].items():
+            for rad in class_id.items():
+                for box in rad.items():
+                    box = box * self.img_size # is a float, maybe to int needed
+                    box_list.append(box)
+                    label_list.append(class_id)
+                    area_list.append((box[2]-box[0])*(box[3]-box[1])) # x_max-x_min * y_max-y_min
+                    iscrowd_list.append(0)
+
+        target = {
+            "boxes": tv.BoundingBoxes(box_list),
+            "labels": label_list,
+            "image_id": idx,
+            "area": torch.tensor(area_list),
+            "iscrowd": torch.tensor(iscrowd_list)
+        }
+
+        return image, target, img_id
 
 
 def load_and_augment_images(pic_folder_path, batch_size, use_normalize=True):
+    # split folders into 70% train and 30% test by ids
+    set_seeds()
+    train_percent = 0.7
+    train_ids = random.sample(os.listdir(pic_folder_path), int(train_percent*len(os.listdir(pic_folder_path))))
+    test_ids = [id for id in os.listdir(pic_folder_path) if id not in train_ids]
+    
+    # normalize on all train images
     if use_normalize:
-        mean, std = get_mean_and_std(str(pic_folder_path))
+        mean, std = get_mean_and_std(pic_folder_path, train_ids, print=False, leave_pbar=True)
         print("Mean: ", mean, ", Std: ", std)
+    
+    # remove file extension
+    train_ids = [id.split(".")[0] for id in train_ids]
+    test_ids = [id.split(".")[0] for id in test_ids]
 
     # Data augmentation and normalization for training
     data_transforms = {
@@ -162,19 +203,66 @@ def load_and_augment_images(pic_folder_path, batch_size, use_normalize=True):
         data_transforms["test"].transforms.append(
             transforms.Normalize(mean=mean, std=std, inplace=True)
         )
+    
+    # load image_dict.json
+    dict_path = 'pre-pro/image_dict.json'
 
-    image_datasets = {
-        x: datasets.ImageFolder(str(pic_folder_path / x), data_transforms[x])
-        for x in ["train", "test"]
-    }
-    dataloaders = {
-        x: torch.utils.data.DataLoader(
-            image_datasets[x], batch_size=batch_size, shuffle=True, num_workers=0
-        )
-        for x in ["train", "test"]
-    }
+    with open(dict_path) as f:
+        train_dict = json.load(f)
 
-    class_names = image_datasets["test"].classes
-    num_classes = len(class_names)
+    # train_dict where keys match train_ids
+    train_dict = {k: train_dict[k] for k in train_ids}
+    test_dict = {k: train_dict[k] for k in test_ids}
+
+    
+
+    # size for images
+    img_size = 224
+    train_dataset = XRayImageDataset(
+        train_dict,
+        img_size,
+        pic_folder_path,
+        mean,
+        std,
+        data_transforms["train"]
+    )
+
+    train_dataloader = torch.utils.data.DataLoader(
+        train_dataset, batch_size=batch_size, shuffle=True, num_workers=0
+    )
+
+    test_dataset = XRayImageDataset(
+        test_dict,
+        img_size,
+        pic_folder_path,
+        mean,
+        std,
+        data_transforms["test"]
+    )
+
+    test_dataloader = torch.utils.data.DataLoader(
+        test_dataset, batch_size=batch_size, shuffle=False, num_workers=0
+    )
+
+    dataloaders = {"train": train_dataloader, "test": test_dataloader}
+
+    class_names = {
+        0: "Aortic enlargement",
+        1: "Atelectasis",
+        2: "Calcification",
+        3: "Cardiomegaly",
+        4: "Consolidation",
+        5: "ILD",
+        6: "Infiltration",
+        7: "Lung Opacity",
+        8: "Nodule/Mass",
+        9: "Other lesion",
+        10: "Pleural effusion",
+        11: "Pleural thickening",
+        12: "Pneumothorax",
+        13: "Pulmonary fibrosis",
+        14: "No finding",
+    }
+    num_classes = class_names.items().__len__()
 
     return dataloaders, class_names, num_classes
